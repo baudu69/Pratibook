5ba0b9740c3bfef45fb6f7b7aa3b802ad9e3ebc8
baudu <bastien.audu@gmail.com>
2022-09-09 09:24:21 +0200

    Init commit

---
+++ pratibook-api/src/test/java/fr/inextenso/pratibook/pratibookapi/PratibookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.inextenso.pratibook.pratibookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PratibookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
