58af3e786235d05bd1ce1b58eb62343087a75815
baudu <bastien.audu@gmail.com>
2022-10-01 12:47:32 +0200

    Connexion

---
+++ pratibook-api/src/main/java/fr/inextenso/pratibook/pratibookapi/model/Role.java
@@ -0,0 +1,32 @@
+package fr.inextenso.pratibook.pratibookapi.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "role_id", nullable = false)
+	private Integer id;
+
+	@Column(name = "name", nullable = false, length = 45)
+	private String name;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
