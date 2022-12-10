9bafcb8d4bd23e44bfd48bd577edf8b2902f6e95
Alexis Giust <alx.giust@gmail.com>
2022-12-10 17:06:11 +0100

    Premier jet liste des auteurs front #6

--- pratibook-ui/src/app/app.route.ts
+++ pratibook-ui/src/app/app.route.ts
@@ -1,8 +1,10 @@
 import {Routes} from "@angular/router";
 import {AccueilComponent} from "./accueil/accueil.component";
+import {ListeAuteursComponent} from "./auteur/liste-auteurs/liste-auteurs.component";
 
 export const appRoutes: Routes = [
   { path: '', component: AccueilComponent },
+  {path: 'auteurs', component: ListeAuteursComponent},
   { path: 'signIn', loadChildren: () => import('./auth/sign-in/sign-in.module').then(mod => mod.SignInModule) },
   { path: 'signUp', loadChildren: () => import('./auth/sign-up/sign-up.module').then(mod => mod.SignUpModule) }
 ]
---
+++ pratibook-ui/src/app/shared/model/igenre.ts
@@ -0,0 +1,4 @@
+export interface IGenre {
+  idGenre: number,
+  nomGenre: string,
+}
